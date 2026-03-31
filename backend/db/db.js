f4fb95373b050ada94b5dc620f95df6c0a40fd09
Arjav Jain <162596890+arjavvjain15@users.noreply.github.com>
2026-03-31 19:08:09 +0530

    Fix typo in mongoose variable name and update connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,10 @@
-const mongoos = require('mongoose');
+const mongoose = require('mongoose');
 const connectDB = async()=>{
     try {
-        await mongoos.connect('mongodb+srv://deadlock:asrasr123@cluster0.mnhdnpb.mongodb.net/?retryWrites=true&w=majority')
-            console.log("Connected!!!")
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected!!!");
     } catch (error) {
-        console.log("Not Connected!!")
+        console.log("ERROR:", error.message);
     }
 }
 module.exports = connectDB
